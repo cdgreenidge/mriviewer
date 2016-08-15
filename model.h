f916151366f354c70a69a18cc86339996a251987
Daniel Greenidge <cdgreenidge@users.noreply.github.com>
2016-08-15 16:14:49 -0400

    Make Model constructor explicit

--- model.h
+++ model.h
@@ -5,7 +5,7 @@
 class Model {
 
 public:
-	Model(size_t x, size_t y, size_t z, size_t t, float scale);
+	explicit Model(size_t x, size_t y, size_t z, size_t t, float scale);
 	size_t x() const noexcept;
 	size_t y() const noexcept;
 	size_t z() const noexcept;
