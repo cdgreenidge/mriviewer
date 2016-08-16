cbddf1bc1bf989aefacb554df582bdebfc25244d
Daniel Greenidge <cdgreenidge@users.noreply.github.com>
2016-08-16 12:03:38 -0400

    Add slicing methods to Mri class

--- mri.cpp
+++ mri.cpp
@@ -5,3 +5,15 @@ Mri::Mri(const float *data, const size_t x, const size_t y,
 		 const size_t z, const size_t t) :
 	data_(data, boost::extents[x][y][z][t], boost::fortran_storage_order())
 { }
+
+Array2 Mri::coronalSlice(const size_t y, const size_t t) const {
+	return data_[boost::indices[Range()][y][Range()][t]];
+}
+
+Array2 Mri::sagittalSlice(const size_t x, const size_t t) const {
+	return data_[boost::indices[x][Range()][Range()][t]];
+}
+
+Array2 Mri::axialSlice(const size_t z, const size_t t) const {
+	return data_[boost::indices[Range()][Range()][z][t]];
+}
--- mri.h
+++ mri.h
@@ -4,7 +4,7 @@
  */
 
 typedef boost::const_multi_array_ref<float, 4> Array4;
-typedef Array4::array_view<2>::type Array2;
+typedef Array4::const_array_view<2>::type Array2;
 typedef boost::multi_array_types::index Index;
 typedef boost::multi_array_types::index_range Range;
 
@@ -13,6 +13,9 @@ class Mri {
 public:
 	explicit Mri(const float * const data, const size_t x, const size_t y,
 				 const size_t z, const size_t t);
+	Array2 coronalSlice(const size_t y, const size_t t) const;
+	Array2 sagittalSlice(const size_t x, const size_t t) const;
+	Array2 axialSlice(const size_t z, const size_t t) const;
 
 private:
 	Array4 data_;
